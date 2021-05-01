e105e4905dc011aa2bccb9bcf3370e94e1f9fbb7
Sekooly <sekooly@gmail.com>
2021-05-02 00:04:41 +0200

    on teste le manifest pour PWA

--- docs/scripts/tele-enseignement.js
+++ docs/scripts/tele-enseignement.js
@@ -11712,3 +11712,97 @@ function canva_scrollable(id_canva){
 	    dragging = false;
 	}, false);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+
+
+/*********************** PWA ***************************/
+window.addEventListener('beforeinstallprompt', (e) => {
+	
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	e.preventDefault();
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
+	// Update UI to notify the user they can add to home screen
+	addBtn.style.display = 'block';
+
+	addBtn.addEventListener('click', (e) => {
+		// hide our user interface that shows our A2HS button
+		addBtn.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+		if (choiceResult.outcome === 'accepted') {
+			console.log('Accepté.');
+		} else {
+			console.log('Refusé.');
+		}
+		deferredPrompt = null;
+		});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/******************** SERVICE WORKER *******************/
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('../sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.error('ServiceWorker registration failed: ', err);
+    });
+  });
+}
