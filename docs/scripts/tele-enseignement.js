7788fa14269181eba4cf4add8cc181c9ef6d9333
Sekooly <sekooly@gmail.com>
2021-05-02 13:36:45 +0200

    pas de print du sw

--- docs/scripts/tele-enseignement.js
+++ docs/scripts/tele-enseignement.js
@@ -11800,7 +11800,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('../sw.js').then(function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      //console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       // registration failed :(
       console.error('ServiceWorker registration failed: ', err);
