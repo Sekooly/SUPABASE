3fdab9876fb9fa82941c3ac99f15a3dca63a191e
Sekooly <sekooly@gmail.com>
2021-05-25 18:41:24 +0200

    cache - resolution?

--- docs/sw.js
+++ docs/sw.js
@@ -1,12 +1,6 @@
 // use a cacheName for cache versioning
-var cacheName = 'v1:static';
-
-// during the install phase you usually want to cache static assets
-self.addEventListener('install', function(e) {
-    // once the SW is installed, go ahead and fetch the resources to make this work offline
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll([
+var cacheName = 'v2:static';
+var liste_des_caches = [
                 'scripts/aes.js',
                 'scripts/hash.js',
                 'scripts/jquery.min.js',
@@ -14,7 +8,18 @@ self.addEventListener('install', function(e) {
                 'scripts/pdf.js',
                 'scripts/pdf.worker.js',
                 'scripts/thenBy.js'               
-            ]).then(function() {
+            ]
+var url_prefix = 'https://sekooly.github.io/SUPABASE/'
+
+if(!window.location.href.includes("hibiscus")) liste_des_caches = liste_des_caches.map(e => url_prefix + e)
+
+
+// during the install phase you usually want to cache static assets
+self.addEventListener('install', function(e) {
+    // once the SW is installed, go ahead and fetch the resources to make this work offline
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(liste_des_caches).then(function() {
                 self.skipWaiting();
             });
         })
