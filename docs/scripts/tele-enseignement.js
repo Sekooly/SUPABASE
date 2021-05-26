75686382f3c9229dc06c1910a963e9a7513ff156
Sekooly <sekooly@gmail.com>
2021-05-26 19:15:30 +0200

    logique inversée... afficher tout possible si au moins 1 masqué

--- docs/scripts/tele-enseignement.js
+++ docs/scripts/tele-enseignement.js
@@ -8938,9 +8938,9 @@ async function un_menu_clic(id_parametre){
 		    elements_menu_haut_avec_tout_voir = elements_menu_haut_avec_tout_voir ?  elements_menu_haut_avec_tout_voir.split(",") : []
 			//if(elements_menu_haut_avec_tout_voir.indexOf(id_parametre + ":") === -1){
 			if (elements_menu_haut_avec_tout_voir.some(e => e.includes(id_parametre+":"))){
-				autoriser_tout_voir(false)
-			}else{
 				autoriser_tout_voir(true)
+			}else{
+				autoriser_tout_voir(false)
 			}
 		}
 	    
