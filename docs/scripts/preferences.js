379033ba97ed27d2388bc718d7fbfaac250cc85d
Sekooly <sekooly@gmail.com>
2021-06-16 13:37:32 +0200

    pas de pref

--- docs/scripts/preferences.js
+++
@@ -1,9 +0,0 @@
-function appliquer_couleurs(){
-	if(data_etablissement['preferences']['couleurs']){
-		console.log(data_etablissement['preferences']['couleurs'])
-		//$("#style-sekooly").remove()
-		element_DOM("custom-css").innerText = JSON.stringify(data_etablissement.preferences.couleurs).replaceAll('"','').replaceAll(':{','{')
-	}
-}
-
-appliquer_couleurs()
